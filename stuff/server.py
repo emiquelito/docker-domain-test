766d3fb7f8b9b9237101669dd9cccffd4e9569ba
wisedrette <ny4koo@gmail.com>
2018-05-22 18:28:16 +0200

    simple flask subdomains app with different output per subdomain

--- stuff/server.py
+++ stuff/server.py
@@ -1,19 +1,39 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, abort
+from jinja2 import TemplateNotFound
 
 app = Flask(__name__)
-app.url_map.host_matching = True
+app.config['SERVER_NAME'] = os.getenv('SQUASH_DOMAIN', 'localhost')
 
 
-@app.route('/', host='<host>')
-def index(host):
-    if 'deployrun.com' in host:
-        return '<body><h2>It is flask domain-specific page.</h2></body>'
-    return '<body>Hello world. <a href="/about/">About this page</a>.</body>'
+@app.route('/')
+def index():
+    domain = os.getenv('SQUASH_DOMAIN', 'localhost')
+    return f'<body>Hello world. <a href="//static.{domain}/">Static page</a>.</body>'
 
 
-@app.route('/about/', host='<host>')
-def about(host):
-    return f'<body>This is the about page for {host}</body>'
+@app.route('/', subdomain='<subdomain>')
+def sub_index(subdomain):
+    if subdomain == 'static':
+        return render_template('static.html')
+    context = {
+        'user': subdomain,
+    }
+    try:
+        return render_template('index.html', **context)
+    except TemplateNotFound:
+        abort(404)
+
+
+@app.route('/about/', subdomain='<subdomain>')
+def about(subdomain):
+    context = {
+        'user': subdomain,
+    }
+    try:
+        return render_template('about.html', **context)
+    except TemplateNotFound:
+        abort(404)
 
 
 if __name__ == "__main__":
